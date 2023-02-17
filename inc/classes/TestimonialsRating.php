5cfbac3534b2e3bd6fac1365516aad4ccf5f7c11
Robert McCormack <robertmccormack147@gmail.com>
2023-02-17 21:54:33 +0000

    simplified-updated-translated

--- inc/classes/TestimonialsRating.php
+++ inc/classes/TestimonialsRating.php
@@ -1,157 +1,109 @@
 <?php
 namespace Rmcc;
+
 use Timber\Timber;
 
-class TestimonialsRating extends Timber {
+class TestimonialsRating extends Timber
+{
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::__construct();
     add_filter('timber/twig', array($this, 'add_to_twig'));
     add_filter('timber/context', array($this, 'add_to_context'));
-    
+
     add_action('plugins_loaded', array($this, 'plugin_timber_locations'));
-    add_action('plugins_loaded', array($this, 'plugin_text_domain_init')); 
+    add_action('plugins_loaded', array($this, 'plugin_text_domain_init'));
     add_action('wp_enqueue_scripts', array($this, 'plugin_enqueue_assets'));
 
     add_filter('the_content', array($this, 'remove_autop_testimonials'), 0);
-    
+
     add_action('init', array($this, 'register_post_types'));
-    add_action('plugins_loaded' , array($this, 'plugin_custom_fields'));
-    
+    // add_action('plugins_loaded', array($this, 'plugin_custom_fields'));
+
     add_shortcode('testimonials_rating_section', 'testimonials_rating_section');
   }
-  
-  public function remove_autop_testimonials($content) {
-    'testimonials' === get_post_type() && remove_filter( 'the_content', 'wpautop' );
+
+  public function remove_autop_testimonials($content)
+  {
+    'testimonials' === get_post_type() && remove_filter('the_content', 'wpautop');
     return $content;
   }
-  
-  public function register_post_types() {
+
+  public function register_post_types()
+  {
     $labels_testimonials = array(
-      'name'                  => _x( 'Testimonials', 'Testimonials label: Plural', 'testimonials-rating' ),
-      'singular_name'         => _x( 'Testimonial', 'Testimonial label: Singular', 'testimonials-rating' ),
-      'menu_name'             => _x( 'Testimonials', 'Testimonials label: Plural', 'testimonials-rating' ),
-      'name_admin_bar'        => _x( 'Testimonial', 'Testimonial label: Singular', 'testimonials-rating' ),
-      'archives'              => _x( 'Testimonials', 'Testimonials label: Archive', 'testimonials-rating' ),
-      'attributes'            => 'Item Attributes',
-      'parent_item_colon'     => 'Parent Item:',
-      'all_items'             => 'All Items',
-      'add_new_item'          => 'Add New Item',
-      'add_new'               => 'Add New',
-      'new_item'              => 'New Item',
-      'edit_item'             => 'Edit Item',
-      'update_item'           => 'Update Item',
-      'view_item'             => 'View Item',
-      'view_items'            => 'View Items',
-      'search_items'          => 'Search Item',
-      'not_found'             => 'Not found',
-      'not_found_in_trash'    => 'Not found in Trash',
-      'featured_image'        => 'Featured Image',
-      'set_featured_image'    => 'Set featured image',
-      'remove_featured_image' => 'Remove featured image',
-      'use_featured_image'    => 'Use as featured image',
-      'insert_into_item'      => 'Insert into item',
-      'uploaded_to_this_item' => 'Uploaded to this item',
-      'items_list'            => 'Items list',
-      'items_list_navigation' => 'Items list navigation',
-      'filter_items_list'     => 'Filter items list',
+      'name' => _x('Testimonials', 'Post Type General Name', 'testimonials-rating'),
+      'singular_name' => _x('Testimonial', 'Post Type Singular Name', 'testimonials-rating'),
+      'menu_name' => __('Testimonials', 'testimonials-rating'),
+      'name_admin_bar' => __('Testimonial', 'testimonials-rating'),
+      'archives' => __('Item Archives', 'testimonials-rating'),
+      'attributes' => __('Item Attributes', 'testimonials-rating'),
+      'parent_item_colon' => __('Parent Item:', 'testimonials-rating'),
+      'all_items' => __('All Items', 'testimonials-rating'),
+      'add_new_item' => __('Add New Item', 'testimonials-rating'),
+      'add_new' => __('Add New', 'testimonials-rating'),
+      'new_item' => __('New Item', 'testimonials-rating'),
+      'edit_item' => __('Edit Item', 'testimonials-rating'),
+      'update_item' => __('Update Item', 'testimonials-rating'),
+      'view_item' => __('View Item', 'testimonials-rating'),
+      'view_items' => __('View Items', 'testimonials-rating'),
+      'search_items' => __('Search Item', 'testimonials-rating'),
+      'not_found' => __('Not found', 'testimonials-rating'),
+      'not_found_in_trash' => __('Not found in Trash', 'testimonials-rating'),
+      'featured_image' => __('Featured Image', 'testimonials-rating'),
+      'set_featured_image' => __('Set featured image', 'testimonials-rating'),
+      'remove_featured_image' => __('Remove featured image', 'testimonials-rating'),
+      'use_featured_image' => __('Use as featured image', 'testimonials-rating'),
+      'insert_into_item' => __('Insert into item', 'testimonials-rating'),
+      'uploaded_to_this_item' => __('Uploaded to this item', 'testimonials-rating'),
+      'items_list' => __('Items list', 'testimonials-rating'),
+      'items_list_navigation' => __('Items list navigation', 'testimonials-rating'),
+      'filter_items_list' => __('Filter items list', 'testimonials-rating'),
     );
     $args_testimonials = array(
-      'label'                 => _x( 'Testimonials', 'Testimonials label: Plural', 'testimonials-rating' ),
-      'description'           => _x( 'Competition Testimonial...', 'Testimonials description', 'testimonials-rating' ),
-      'labels'                => $labels_lists,
-      'supports'              => array( 'title', 'editor', 'revisions', 'custom-fields' ),
-      'hierarchical'          => false,
-      'public'                => true,
-      'show_ui'               => true,
-      'show_in_menu'          => true,
-      'menu_position'         => 4,
-      'show_in_admin_bar'     => true,
-      'show_in_nav_menus'     => true,
-      'can_export'            => true,
-      'has_archive'           => false,
-      'exclude_from_search'   => true,
-      'publicly_queryable'    => true,
-      'query_var'             => false,
-      'capability_type'       => 'page',
+      'label' => __('Testimonial', 'testimonials-rating'),
+      'description' => __('Testimonial Description', 'testimonials-rating'),
+      'labels' => $labels_testimonials,
+      'supports' => array('title', 'editor', 'revisions', 'custom-fields'),
+      'hierarchical' => false,
+      'public' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'menu_position' => 4,
+      'show_in_admin_bar' => true,
+      'show_in_nav_menus' => true,
+      'can_export' => true,
+      'has_archive' => false,
+      'exclude_from_search' => true,
+      'publicly_queryable' => true,
+      'query_var' => false,
+      'capability_type' => 'page',
     );
-    register_post_type( 'testimonials', $args_testimonials );
+    register_post_type('testimonials', $args_testimonials);
   }
-  public function plugin_custom_fields() {
-    if( function_exists('acf_add_local_field_group') ):
-    
-    acf_add_local_field_group(array(
-    	'key' => 'group_60d37f3ba214d',
-    	'title' => 'Testimonials',
-    	'fields' => array(
-    		array(
-    			'key' => 'field_60d37f52d3310',
-    			'label' => 'Testimonial Rating',
-    			'name' => 'testimonial_rating',
-    			'type' => 'number',
-    			'instructions' => '',
-    			'required' => 0,
-    			'conditional_logic' => 0,
-    			'wrapper' => array(
-    				'width' => '',
-    				'class' => '',
-    				'id' => '',
-    			),
-    			'default_value' => 5,
-    			'placeholder' => '',
-    			'prepend' => '',
-    			'append' => '',
-    			'min' => 1,
-    			'max' => 5,
-    			'step' => 1,
-    		),
-    	),
-    	'location' => array(
-    		array(
-    			array(
-    				'param' => 'post_type',
-    				'operator' => '==',
-    				'value' => 'testimonials',
-    			),
-    		),
-    	),
-    	'menu_order' => 0,
-    	'position' => 'normal',
-    	'style' => 'default',
-    	'label_placement' => 'top',
-    	'instruction_placement' => 'label',
-    	'hide_on_screen' => '',
-    	'active' => true,
-    	'description' => '',
-    ));
-    
-    endif;
+  public function plugin_custom_fields()
+  {
+
   }
-  
-  public function plugin_timber_locations() {
+
+  public function plugin_timber_locations()
+  {
     // if timber::locations is empty (another plugin hasn't already added to it), make it an array
-    if(!Timber::$locations) Timber::$locations = array();
+    if (!Timber::$locations)
+      Timber::$locations = array();
     // add a new views path to the locations array
     array_push(
-      Timber::$locations, 
+      Timber::$locations,
       TESTIMONIALS_RATING_PATH . 'views'
     );
   }
-  public function plugin_text_domain_init() {
-    load_plugin_textdomain('testimonials-rating', false, TESTIMONIALS_RATING_BASE. '/languages');
+  public function plugin_text_domain_init()
+  {
+    load_plugin_textdomain('testimonials-rating', false, TESTIMONIALS_RATING_BASE . '/languages');
   }
-  public function plugin_enqueue_assets() {
-    wp_enqueue_style(
-      'swiper-js',
-      TESTIMONIALS_RATING_URL . 'public/css/swiper-bundle.min.css'
-    );
-    wp_enqueue_script(
-      'swiper-js',
-      TESTIMONIALS_RATING_URL . 'public/js/swiper-bundle.min.js',
-      '',
-      '1.0.0',
-      true
-    );
+  public function plugin_enqueue_assets()
+  {
     wp_enqueue_style(
       'testimonials-rating',
       TESTIMONIALS_RATING_URL . 'public/css/testimonials-rating.css',
@@ -164,15 +116,47 @@ class TestimonialsRating extends Timber {
       '1.0.0',
       true
     );
+    wp_enqueue_style(
+      'swiper',
+      'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css'
+    );
+    wp_enqueue_script(
+      'swiper',
+      'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js',
+      '',
+      '8.0.0',
+      false
+    );
+    wp_enqueue_style(
+      'uikit',
+      'https://cdn.jsdelivr.net/npm/uikit@3.15.24/dist/css/uikit.min.css'
+    );
+    wp_enqueue_script(
+      'uikit',
+      'https://cdn.jsdelivr.net/npm/uikit@3.15.24/dist/js/uikit.min.js',
+      array(),
+      '3.15.24',
+      false
+    );
+    wp_enqueue_script(
+      'uikit-icons',
+      'https://cdn.jsdelivr.net/npm/uikit@3.15.24/dist/js/uikit-icons.min.js',
+      array(),
+      '3.15.24',
+      false
+    );
   }
-  
-  public function add_to_twig($twig) { 
-    if(!class_exists('Twig_Extension_StringLoader')){
+
+  public function add_to_twig($twig)
+  {
+    if (!class_exists('Twig_Extension_StringLoader')) {
       $twig->addExtension(new \Twig_Extension_StringLoader());
     }
     return $twig;
   }
-  public function add_to_context($context) {
-    return $context;    
+  public function add_to_context($context)
+  {
+    return $context;
   }
+  
 }
